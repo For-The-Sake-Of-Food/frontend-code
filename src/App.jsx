4abb839f4db09a622528840f9501784840d567ca
AgumaDestiny <91542728+AgumaDestiny@users.noreply.github.com>
2023-10-25 10:43:07 +0300

    our about us page code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import About from "./Components/About"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+ 
+function App() { 
   return (
     <>
-      <div>
-        <h1>
-          Hello World
-        </h1>
+      <div className='App'>
+        <About/>
       </div>
     </>
   )
