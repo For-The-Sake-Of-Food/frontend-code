e233da1002dee774b7aa0781dded3066a00454e2
KatukundaRochelle <katukundarochelle16@gmail.com>
2023-11-01 11:38:15 +0300

    dessert image

--- for-the-sake-of-food/src/components/Dessertcard.js
+++ for-the-sake-of-food/src/components/Dessertcard.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Dessertcard = () => {
   return (
     <div className="max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-      <Image src="/dessert-category.jpg" alt="Dessert" width= {100} height={100}className="w-full h-32 object-cover" />
+      <Image src="/dessert.jpg" alt="Dessert" width= {100} height={100}className="w-full h-32 object-cover" />
       <div className="p-4 bg-[#E7F9FD] rounded-lg">
         <h2 className="text-2xl font-semibold text-center text-black">Dessert</h2>
         <Link href="/dessert">
