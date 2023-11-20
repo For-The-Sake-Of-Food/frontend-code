6134443ba0322f6b625bb9ed9a1a00d86b374e70
KatukundaRochelle <katukundarochelle16@gmail.com>
2023-11-20 22:58:27 +0300

    changes

--- for-the-sake-of-food/src/components/Card.js
+++ for-the-sake-of-food/src/components/Card.js
@@ -7,8 +7,8 @@ const Card = ({ image, timestamp, description, link, id, category, title }) => {
     <div className="max-w-lg rounded overflow-hidden bg-[#E7F9FD] shadow-lg m-4">
       <Image width={450} height={50} src={image} alt="Card Image" className="" />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{timestamp}</div>
-        <p className="text-gray-700 text-base">{title}</p>
+        
+        <p className="text-gray-700 font-bold text-xl">{title}</p>
       </div>
       <div className="px-6 py-4">
         <Link href={`/${category}/${id}`}>
