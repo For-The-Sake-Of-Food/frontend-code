ec94998bd398d652bb08c21b6e5f626784ffbe1e
Moses <email@email.com>
2024-02-08 18:27:19 +0300

    adjusted background for signin/signup forms

--- for-the-sake-of-food/src/app/sign-in/[[...sign-in]]/page.js
+++ for-the-sake-of-food/src/app/sign-in/[[...sign-in]]/page.js
@@ -2,8 +2,15 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center pt-20">
-      <SignIn />;
+    <div className="flex justify-center items-center pt-20"
+    style={{
+      backgroundImage: 'url("./signin.jpg")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '100vh', // Optional: Set a minimum height to cover the entire viewport
+    }}
+    >
+      <SignIn />
     </div>
   );
 }
--- for-the-sake-of-food/src/app/sign-up/[[...sign-up]]/page.js
+++ for-the-sake-of-food/src/app/sign-up/[[...sign-up]]/page.js
@@ -2,7 +2,14 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center pt-20">
+    <div className="flex justify-center items-center pt-20"
+    style={{
+      backgroundImage: 'url("./signin.jpg")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '100vh', // Optional: Set a minimum height to cover the entire viewport
+    }} 
+    >
       <SignUp />
     </div>
   );
