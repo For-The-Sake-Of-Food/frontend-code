164db110e043363d6b399ca47669852a746b203d
Moses <email@email.com>
2024-02-13 22:26:45 +0300

    minor changes

--- for-the-sake-of-food/src/app/meal-planner/page.js
+++ for-the-sake-of-food/src/app/meal-planner/page.js
@@ -1,7 +1,9 @@
-const Mealplanner = ()=>{
-    return <div>
-        <h1 className="text-center flex justify-center">Meal Planner</h1>
+const Mealplanner = () => {
+  return (
+    <div>
+      <h1 className="text-center flex justify-center pt-20 text-black">Meal Planner</h1>
     </div>
-}
+  );
+};
 
 export default Mealplanner;
