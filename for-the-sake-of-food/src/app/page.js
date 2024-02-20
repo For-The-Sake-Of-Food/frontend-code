fc851cdff302ada17439cc4d4d4ff6bb2665dd1d
AgumaDestiny <91542728+AgumaDestiny@users.noreply.github.com>
2024-02-20 03:50:05 +0300

    modal complete

--- for-the-sake-of-food/src/app/page.js
+++ for-the-sake-of-food/src/app/page.js
@@ -5,9 +5,78 @@ import Juicecard from "@/components/Juicecard";
 import Food from "@/app/food/page";
 import Image from "next/image";
 import { useUser } from "@clerk/nextjs";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
 
 const Welcome = () => {
   const { user } = useUser();
+  const [dialogOpen, setDialogOpen] = useState(true);
+
+  const [formData, setFormData] = useState({
+    age: "",
+    weight: "",
+    height: "",
+    bmi: "",
+    conditions: {
+      diabetes: false,
+      highBloodPressure: false,
+      highCholesterol: false,
+      none: false,
+    },
+  });
+
+  const calculateBMI = () => {
+    const { age, weight, height } = formData;
+    const bmiValue = (weight / Math.pow(height / 100, 2)).toFixed(2);
+    setFormData((prevData) => ({ ...prevData, bmi: bmiValue }));
+  };
+
+  const conditionLabels = [
+    { label: 'Diabetes', value: 'diabetes' },
+    { label: 'High Blood Pressure', value: 'highBloodPressure' },
+    { label: 'High Cholesterol Levels', value: 'highCholesterol' },
+    { label: 'None', value: 'none' },
+  ];
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === "checkbox") {
+      setFormData((prevData) => {
+        const updatedConditions = { ...prevData.conditions, [name]: checked };
+
+        if (
+          ["diabetes", "highBloodPressure", "highCholesterol"].includes(name)
+        ) {
+          updatedConditions.none = false;
+        } else if (name === "none") {
+          updatedConditions.diabetes = false;
+          updatedConditions.highBloodPressure = false;
+          updatedConditions.highCholesterol = false;
+        }
+
+        return { ...prevData, conditions: updatedConditions };
+      });
+    } else {
+      setFormData((prevData) => ({ ...prevData, [name]: value }));
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  const inputStyle = "p-2 border border-gray-300 rounded-md w-full";
+
+  const conditionLabelStyle = "block mb-2";
+
   return (
     <div>
       <div className="relative rounded-lg overflow-hidden pl-4">
@@ -25,9 +94,7 @@ const Welcome = () => {
                   Hello, {user.fullName}.
                 </p>
               ) : (
-                <p className="text-5xl font-bold mb-2">
-                  Hey There!
-                </p>
+                <p className="text-5xl font-bold mb-2">Hey There!</p>
               )}
               <p className="text-lg">
                 Welcome to{" "}
@@ -37,23 +104,6 @@ const Welcome = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className="flex flex-col pl-4">
-        <h1 className="text-5xl pl-4 py-10 pt-20 md:pt-35 text-black font-bold">
-          Meal Categories
-        </h1>
-        <div className="flex flex-col pb-5 items-center gap-8 md:flex-row md:justify-evenly md:gap-8">
-          <div className="w-full md:w-1/3 lg:w-3/4 max-w-screen-md max-h-55">
-            <Foodcard />
-          </div>
-          <div className="w-full md:w-1/3 lg:w-3/4 max-w-screen-md">
-            <Juicecard />
-          </div>
-          <div className="w-full md:w-1/3 lg:w-3/4 max-w-screen-md">
-            <Dessertcard />
-          </div>
-        </div>
-      </div> */}
 
       <div className="flex flex-col pl-8 pr-2">
         <h1 className="text-4xl pl-4 py-10 pt-20 md:pt-35 text-black font-bold">
@@ -72,49 +122,102 @@ const Welcome = () => {
         </div>
       </div>
 
-      {/* <div className="flex flex-col md:flex-row items-center ">
-        <div className="w-full md:w-1/2 p-6 md:order-1">
-          <Image
-            src="/user.jpg"
-            alt="user image"
-            width={700}
-            height={500}
-            className="rounded-md"
-          />
-        </div>
-        <div className="w-full pl-10 md:w-1/2 p-4 md:order-2">
-          <h1 className="text-2xl pl-10 pr-5 font-bold relative">
-            <span className="text-black">Why </span>
-            <span className="text-[#3A8700] font-bold">Our Recipes</span>
-            <span className="text-black"> Are The Best</span>
-            <span className="absolute top-[-2.5rem] left-[-3rem] text-gray-300 text-9xl ml-4">
-              “
-            </span>
-          </h1>
-          <p className="pl-10 pr-5">
-            At 65 its become more important than ever to make healthy food
-            choices. This app has opened a world of delicious nutritious meals
-            that keep me feeling vibrant and energized. Its not just an app its
-            a companion on my journey to a healthier happier life. The recipes
-            are easy to follow and I have never had so much fun experimenting in
-            the kitchen. Thank you for making healthy eating a joy!
-          </p>
-          <p className="pl-10 pr-5">~Carol Lynn</p>
-        </div>
-      </div> */}
-
       <div className="mt-5">
         <Food />
       </div>
+      <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+            <div className="mb-4 ">
+              <label>
+                Age:
+                <input
+                  type="number"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleChange}
+                  required
+                  className={inputStyle}
+                />
+              </label>
+            </div>
+
+            <div className="mb-4">
+              <label>
+                Weight (kg):
+                <input
+                  type="number"
+                  name="weight"
+                  value={formData.weight}
+                  onChange={handleChange}
+                  required
+                  className={inputStyle}
+                />
+              </label>
+            </div>
+
+            <div className="mb-4">
+              <label>
+                Height (cm):
+                <input
+                  type="number"
+                  name="height"
+                  value={formData.height}
+                  onChange={handleChange}
+                  required
+                  className={inputStyle}
+                />
+              </label>
+            </div>
+
+            <div className="mb-4">
+              <button
+                type="button"
+                onClick={calculateBMI}
+                className="bg-black hover:bg-[#858684] text-white font-bold py-2 px-4 rounded-full mb-4"
+              >
+                Calculate BMI
+              </button>
+              {formData.bmi && <p className="mt-2">Your BMI: {formData.bmi}</p>}
+            </div>
+
+            <div className="mb-4">
+              <label className={conditionLabelStyle}>
+                Conditions:
+                <div className="mt-2 ml-4">
+                  {conditionLabels.map((condition) => (
+                    <label
+                      key={condition.value}
+                      className={conditionLabelStyle}
+                    >
+                      <input
+                        type="checkbox"
+                        name={condition.value}
+                        checked={formData.conditions[condition.value]}
+                        onChange={handleChange}
+                        className="mr-2 !checked:bg-black" 
+                      />
+                      {condition.label}
+                    </label>
+                  ))}
+                </div>
+              </label>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="bg-black hover:bg-[#858684] text-white font-bold py-2 px-4 rounded-full mb-4"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
 
 export default Welcome;
-// "use-client";
-// const Home=()=>{
-//   return(
-//     <h1 className="text-black pt-40 mt-40 text-center justify-center">this is home</h1>
-//   )
-// }
-// export default Home;
--- for-the-sake-of-food/src/components/Card.js
+++ for-the-sake-of-food/src/components/Card.js
@@ -1,14 +1,6 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
 
 const Card = ({
   image,
@@ -44,19 +36,6 @@ const Card = ({
         >
           Read More
         </Link>
-        <Dialog>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>
-
       </div>
     </div>
   );
